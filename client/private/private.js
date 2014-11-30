9c1fb321f89db83df7a7c49ee4f10cac4848c896
eganpg <eganpg@gmail.com>
2014-11-30 14:30:12 -0800

    added second row, need onclick refactoring

--- client/private/private.js
+++ client/private/private.js
@@ -32,7 +32,6 @@ var checkout = StripeCheckout.configure({
       else {
         alert('you need to enter a valid referral code');
       }
-
     }
   })
 
@@ -49,7 +48,6 @@ var checkout = StripeCheckout.configure({
       boards: function() {
       userid = Meteor.user()._id;
       return Privateboard.find({ user: userid });
-      
     }
   });
 
@@ -102,16 +100,31 @@ var checkout = StripeCheckout.configure({
   });
 
 
+  Template.PrivateDetail.rendered = function() {
+    var game_id = window.location.pathname;
+    var gamerid = game_id.substring(9);
+    var picked = Privatesquare.find({game_id: gamerid}).count();
+    console.log(picked);
+    for(i=0; i<picked; i++){
+      var btn = '.btn' + (i+1);
+      console.log(btn);
+      $(btn).css({"backgroundColor":"white","color":"black"});
+    }
+    
+  }
+
 // Login for owning a square
+
   Template.PrivateDetail.events({
     'click .btn1': function() {
       var value = $('.btn1').val();
       var game_id = window.location.pathname;
-      var gamerid = game_id.substring(8);
+      var gamerid = game_id.substring(9);
       var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
       console.log(gamerid);
       console.log(value1);
       if(!value1) {
+        $(".btn1").css({"backgroundColor":"white","color":"black"});
         Privatesquare.insert({
           square_id: $('.btn1').val(),
           user: Meteor.user()._id,
@@ -123,17 +136,364 @@ var checkout = StripeCheckout.configure({
       }
     },
     'click .btn2': function() {
-      alert('adding an event');
-      Publicsquare.insert({
-        square_id: $('.btn2').val(),
-        user: Meteor.user()._id
-      });
+      var value = $('.btn2').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn2").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn2').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
     },
     'click .btn3': function() {
-      alert('adding an event');
-      Publicsquare.insert({
-        square_id: $('.btn3').val(),
-        user: Meteor.user()._id
-      });
-    }
+      var value = $('.btn3').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn3").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn3').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn4': function() {
+      var value = $('.btn4').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn4").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn4').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn5': function() {
+      var value = $('.btn5').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn5").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn5').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn6': function() {
+      var value = $('.btn6').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn6").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn6').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn7': function() {
+      var value = $('.btn7').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn7").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn7').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn8': function() {
+      var value = $('.btn8').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn8").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn8').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn9': function() {
+      var value = $('.btn9').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn9").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn9').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn10': function() {
+      var value = $('.btn10').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn10").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn10').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn11': function() {
+      var value = $('.btn11').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn11").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn11').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn12': function() {
+      var value = $('.btn12').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn12").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn12').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn13': function() {
+      var value = $('.btn13').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn13").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn13').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn14': function() {
+      var value = $('.btn14').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn14").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn14').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn15': function() {
+      var value = $('.btn15').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn15").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn15').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn16': function() {
+      var value = $('.btn16').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn16").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn16').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn17': function() {
+      var value = $('.btn17').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn17").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn17').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn18': function() {
+      var value = $('.btn18').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn18").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn18').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn19': function() {
+      var value = $('.btn19').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn19").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn19').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
+    'click .btn20': function() {
+      var value = $('.btn20').val();
+      var game_id = window.location.pathname;
+      var gamerid = game_id.substring(9);
+      var value1 = Privatesquare.findOne({square_id: value,game_id: gamerid});
+      console.log(gamerid);
+      console.log(value1);
+      if(!value1) {
+        $(".btn20").css({"backgroundColor":"white","color":"black"});
+        Privatesquare.insert({
+          square_id: $('.btn20').val(),
+          user: Meteor.user()._id,
+          game_id: gamerid
+          });
+      }
+      else {
+        alert('This Square has Already been Boughten');
+      }
+    },
   });
